1e0d7434fa02118fc747eedc75ec2934cd99fd02
mrlinnth <mrlinnth@users.noreply.github.com>
2022-09-22 11:28:23 +0630

    Update service and order noti details

--- src/modules/noti/NotiOrder.tsx
+++ src/modules/noti/NotiOrder.tsx
@@ -1,7 +1,42 @@
 export const NotiOrder = () => {
   return (
-    <div className="p-4 grid gap-4">
-      Noti order
+    <div className="p-4">
+      <div className="card card-compact bg-base-100 border border-base-300 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">#ABCD12</h2>
+          <div className="overflow-x-auto">
+            <table className="table table-compact w-full">
+              <tbody>
+                <tr>
+                  <th>1</th> 
+                  <td>Item A</td> 
+                  <td>x 1</td>
+                  <td>
+                    <input type="checkbox" className="toggle toggle-sm" />
+                  </td>
+                </tr>
+                <tr>
+                  <th>2</th> 
+                  <td>Item B</td> 
+                  <td>x 2</td>
+                  <td>
+                    <input type="checkbox" className="toggle toggle-sm" />
+                  </td>
+                </tr>
+              </tbody> 
+            </table>
+          </div>
+          <div className="card-actions">
+            <button className="btn btn-block">Accept</button>
+          </div>
+          <div className="card-actions">
+            <button className="btn btn-block">Print</button>
+          </div>
+          <div className="card-actions">
+            <button className="btn btn-block">Go to details</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/modules/noti/NotiService.tsx
+++ src/modules/noti/NotiService.tsx
@@ -1,7 +1,15 @@
 export const NotiService = () => {
   return (
-    <div className="p-4 grid gap-4">
-      Noti service
+    <div className="p-4">
+      <div className="card card-compact bg-base-100 border border-base-300 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Table 1</h2>
+          <p>Call Waiter <span className="ml-2 text-slate-400">5 mins ago</span></p>
+          <div className="card-actions justify-end">
+            <button className="btn">Accept</button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
