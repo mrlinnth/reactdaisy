7db76669e0774327df22c1f8cc9fcfece661bfa9
mrlinnth <mrlinnth@users.noreply.github.com>
2022-09-22 11:42:23 +0630

    Update noti setting screen

--- src/modules/noti/NotiSetting.tsx
+++ src/modules/noti/NotiSetting.tsx
@@ -1,7 +1,29 @@
 export const NotiSetting = () => {
   return (
-    <div className="p-4 grid gap-4">
-      Noti setting
+    <div className="p-4">
+      <div className="card card-compact bg-base-100 border border-base-300 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Subscribe to</h2>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">Table Service</span> 
+              <input type="checkbox" checked className="checkbox" />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">Food Station</span> 
+              <input type="checkbox" checked className="checkbox" />
+            </label>
+          </div>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">Drink Station</span> 
+              <input type="checkbox" checked className="checkbox" />
+            </label>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
